f8520283f0278475bae0f581b1f5c7e1883a5226
Abanoub-Samy13 <201827144@std.sci.cu.edu.eg>
2023-03-31 21:33:35 +0200

    add style forget password and forget password

--- styles/forgetPasswordStyle.js
+++ styles/forgetPasswordStyle.js
@@ -4,7 +4,52 @@ const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
-//put code here
+    img: {
+        height: screenHeight,
+        width: screenWidth,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      container: {
+        flex: 1,
+        top:70,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      input: {
+        width:220,
+        height: 40,
+        margin: 16,
+        borderWidth: 2,
+        padding: 15,
+        color:'white',
+        borderRadius:15,
+        backgroundColor:'#303030',
+        border:0
+      },
+      text1:{
+        fontSize:30,
+        color:'white',
+        margin:15
+      },
+      text2:{
+        fontSize:10,
+        color:'#FF7F50'
+      },
+      text:{
+        fontSize:20,
+        color:'white'
+      },
+      button:{
+        backgroundColor:'#FF7F50',
+        height: 40,
+        width:220,
+        padding: 10,
+        margin: 16,
+        borderRadius:15,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
 });
 
 export default styles;
