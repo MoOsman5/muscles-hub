6e3161c7c2aac6329cc3fc406a2246f9f2873800
Mostafa-Abdelfatah <mostafaelweshy777@gmail.com>
2023-03-31 05:20:43 +0200

    I will make branch

--- auth/Register.js
+++ auth/Register.js
@@ -4,28 +4,7 @@ import styles from '../styles/registerStyle';
 
 const Register = ({navigation}) => {
   return (
-    <View>
-      <ImageBackground
-        source={require('../images/register.png')}
-        resizeMode="stretch"
-        style={styles.img}>
-        <View style={styles.container}>
-          <Text style={styles.text1}>Creat new Account</Text>
-          <TouchableOpacity
-            onPress={()=>navigation.navigate("ForgetPassword")}
-          >
-          <Text style={styles.text2}>Already Registered?Log in here.</Text>
-          </TouchableOpacity>
-          <TextInput placeholder="Name" style={styles.input} />
-          <TextInput placeholder="Email" style={styles.input} />
-          <TextInput placeholder="Password" style={styles.input} />
-          <TextInput placeholder="Comfirm Password" style={styles.input} />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.text}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-    </View>
+    //but code here
   );
 }
 
