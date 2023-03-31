9ce7c5bb1e6654c8e5c1ac6e23ab184f0ede88d9
MoOsman5 <mohamed1592000gmail.com>
2023-03-31 04:07:26 +0200

    creat forgetPassword file

---
+++ auth/ForgetPassword.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, TextInput, ImageBackground,TouchableOpacity  } from 'react-native';
+import styles from '../styles/forgetPasswordStyle';
+  
+
+const ForgetPassword = () => {
+  return (
+        //but code here 
+  );
+}
+
+export default ForgetPassword;
