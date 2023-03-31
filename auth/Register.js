ac456d63e2fe1f73db49e00e3249364d03d8dac9
MoOsman5 <mohamed1592000gmail.com>
2023-03-31 04:13:07 +0200

    create Register file

---
+++ auth/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View, TextInput, ImageBackground,TouchableOpacity  } from 'react-native';
+import styles from '../styles/registerStyle';
+
+const Register = ({navigation}) => {
+  return (
+    //but code here
+  );
+}
+
+export default Register;
