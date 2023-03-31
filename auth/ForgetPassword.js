1f08ffb8d5f21109e4b9e886d5db52f8f3c541ec
MoOsman5 <mohamed1592000gmail.com>
2023-03-31 23:59:18 +0200

    edit forget password style

--- auth/ForgetPassword.js
+++ auth/ForgetPassword.js
@@ -5,7 +5,21 @@ import styles from '../styles/forgetPasswordStyle';
 
 const ForgetPassword = () => {
   return (
-        //but code here 
+        <View>
+          <ImageBackground
+            source={require('../images/resetPassword.png')}
+            resizeMode="stretch"
+            style={styles.img}>
+            <View style={styles.container}>
+              <Text style={styles.text1}>Reset Password</Text>
+              <Text style={styles.text2}>Enter your Email to reset.</Text>
+              <TextInput placeholder="Email" style={styles.input} />
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.text}>Reset</Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+        </View>
   );
 }
 
