9856e4b43a2b23db7a96be809a03e78ecf479c2d
MoOsman5 <mohamed1592000gmail.com>
2023-03-31 04:08:30 +0200

    create forgetPasswordStyle file

---
+++ styles/forgetPasswordStyle.js
@@ -0,0 +1,10 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+//but code here
+});
+
+export default styles;
