3e2ca876eb65dc8c7d766c45c154742952d19b0c
MoOsman5 <mohamed1592000gmail.com>
2023-03-31 04:12:36 +0200

    ceate Login file

---
+++ auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View, TextInput, ImageBackground,TouchableOpacity  } from 'react-native';
+import styles from '../styles/loginStyle';
+import ForgetPassword from './ForgetPassword';
+
+const Login = ({navigation}) => {
+  return (
+    //but code here
+  );
+}
+
+export default Login;
