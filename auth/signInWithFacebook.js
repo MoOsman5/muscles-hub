c96dafa47fa37f7c8ec6de66cd55ce2afd658b39
MoOsman5 <mohamed1592000gmail.com>
2023-04-03 01:44:19 +0200

    create login with facebook

---
+++ auth/signInWithFacebook.js
@@ -0,0 +1,32 @@
+import { auth,providerf} from '../config/firebase';
+import {  signInWithPopup,FacebookAuthProvider} from "firebase/auth";
+
+// signIn With Facebook
+
+const signInWithFacebook =()=>{
+  signInWithPopup(auth, providerf)
+  .then((result) => {
+    // The signed-in user info.
+    const user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const accessToken = credential.accessToken;
+
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
+  });
+}
+
+export{signInWithFacebook};
