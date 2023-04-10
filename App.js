f8cb8a93c1cbb07fd857b77c95f7fcfe689e763c
MoOsman5 <mohamed1592000gmail.com>
2023-04-11 00:42:56 +0200

    add products stacks

--- App.js
+++ App.js
@@ -7,6 +7,15 @@ import Register from './auth/Register'
 import ForgetPassword from './auth/ForgetPassword'
 import Tabs from './components/Tabs';
 // import splashScreen from './components/splashScreen';
+import Shop from './components/Shop';
+import Protien from './Products/protien';
+import Creatine from './Products/creatine';
+import lArginine from './Products/lArginine';
+import Glutamine from './Products/glutamine';
+import Omega3 from './Products/Omega3';
+import Carb from './Products/Carb';
+import Vitamins from './Products/Vitamins';
+import FitEquipment from './Products/fitEquipment';
 
 const Stack = createNativeStackNavigator();
 
@@ -37,6 +46,51 @@ export default function App() {
         <Stack.Screen
           name="Tabs"
           component={Tabs}
+          options={{ headerShown: false }}
+        />
+          <Stack.Screen
+          name="Shop"
+          component={Shop}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Protien"
+          component={Protien}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Creatine"
+          component={Creatine}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="lArginine"
+          component={lArginine}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Glutamine"
+          component={Glutamine}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Omega3"
+          component={Omega3}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Carb"
+          component={Carb}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Vitamins"
+          component={Vitamins}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="FitEquipment"
+          component={FitEquipment}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
