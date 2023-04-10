5870cdbc7ff44e6d0c9e15c83d79abd018b3d7eb
MoOsman5 <mohamed1592000gmail.com>
2023-04-10 22:06:59 +0200

    create profile padge

---
+++ components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+
+const Profile = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'#303030' }}>
+    <Text>Profile Screen</Text>
+  </View>
+  );
+}
+
+
+export default Profile;
