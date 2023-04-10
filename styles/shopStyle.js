3edcda42bedfde1ab86e8374b601a8912e1a0eb6
hagarAbdelazem <126593453+hagarAbdelazem@users.noreply.github.com>
2023-04-10 22:59:24 +0100

    adding shop

--- styles/shopStyle.js
+++ styles/shopStyle.js
@@ -3,6 +3,43 @@ import {StyleSheet, Dimensions} from 'react-native';
 
 
 const styles = StyleSheet.create({
-// code
+    containr:{
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor:'#303030'
+      },
+      mainText:{
+        fontSize:30,
+        color:'white',
+        margin:10,
+        marginTop:28
+      },
+      containr2:{
+        width:'85%',
+        height:100,
+        flexDirection:'row',
+        margin:20
+      },
+      containr3:{
+        width:'50%',
+        height:120,
+        margin:8,
+        alignItems: 'center',
+        borderRadius:20,
+        border:1,
+        borderStyle:'solid',
+        borderColor:'#FF7F50',
+        backgroundColor:'#FF7F50'
+      },
+      img:{
+        width:'100%',
+        height:80,
+        borderRadius:20,
+        border:1
+      },
+      bText:{
+        fontSize:25,
+        color:'white'
+      }
 });
 export default styles;
