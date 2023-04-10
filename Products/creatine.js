e470869b72bc37397e9d37480f16e9f33453c7bf
MoOsman5 <mohamed1592000gmail.com>
2023-04-11 00:43:58 +0200

    create creatine file

---
+++ Products/creatine.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
+const productImage = require('../images/logo.png');
+import { AntDesign,Ionicons } from '@expo/vector-icons'; 
+
+
+
+
+
+<Image source={{uri:'https://th.bing.com/th/id/R.3bcdb103017e2f4b12f1e2c8b9604e78?rik=W8G8Mg2qqPUp2w&pid=ImgRaw&r=0'}} style={{ width: 200, height: 200 }} />
+
+export default class Creatine extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {                                                                          
+      name: 'creatine monohydrate',
+      price: '1000',
+      description: 'This is a beautiful gemstone.',
+    };
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+       {/* code  */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'#303030'
+  },
+
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color:'white'
+  },
+  price: {
+    fontSize: 20,
+    marginBottom: 10,
+    color:'white'
+  },
+  description: {
+    fontSize: 18,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    color:'white'
+  },
+  button: {
+    backgroundColor: '#FF7F50',
+    marginBottom: 40,
+    color:'white',
+    
+  },
+});
