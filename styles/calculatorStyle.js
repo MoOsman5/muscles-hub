fd776dbf8404f9c9399f9304688bbc2069418bc1
Abanoub-Samy13 <120567746+Abanoub-Samy13@users.noreply.github.com>
2023-04-11 01:57:33 +0200

    Calculator Style

--- styles/calculatorStyle.js
+++ styles/calculatorStyle.js
@@ -3,6 +3,132 @@ import {StyleSheet, Dimensions} from 'react-native';
 
 
 const styles = StyleSheet.create({
-//code
+    container: {
+        flex: 1,
+        backgroundColor:'#303030',
+        alignItems: 'center',
+      },
+      cont2:{
+        width:125,
+        margin:0
+      },
+      input: {
+        marginLeft:0,
+        width:115,
+        height: 40,
+        margin: 8,
+        borderWidth: 2,
+        padding: 15,
+        color:'white',
+        borderRadius:15,
+        backgroundColor:'#505050',
+        border:0
+      },
+      gen:{
+        marginLeft:0,
+        width:110,
+        height: 40,
+        margin: 8,
+        borderWidth: 2,
+        padding: 15,
+        color:'white',
+        borderRadius:15,
+        backgroundColor:'#505050',
+        border:0
+    
+      },
+      gen1:{
+        marginLeft:0,
+        width:110,
+        height:100,
+        margin: 8,
+        borderWidth: 2,
+        padding: 15,
+        color:'white',
+        borderRadius:15,
+        backgroundColor:'#505050',
+        border:0
+    
+      },
+      rate:{
+        marginLeft:0,
+        width:250,
+        height: 40,
+        margin: 8,
+        borderWidth: 2,
+        padding: 15,
+        color:'white',
+        borderRadius:15,
+        backgroundColor:'#505050',
+        border:0
+    
+      },
+      rate1:{
+        marginLeft:0,
+        width:250,
+        height:100,
+        margin: 8,
+        borderWidth: 2,
+        padding: 15,
+        color:'white',
+        borderRadius:15,
+        backgroundColor:'#505050',
+        border:0,
+        
+    
+      },
+      text1:{
+        fontSize:26,
+        color:'white',
+        margin:12,
+        marginTop:5
+      },
+      text2:{
+        fontSize:10,
+        color:'#FF7F50'
+      },
+      text3:{
+        fontSize:17,
+        color:'white',
+        margin:10
+      },
+      text:{
+        fontSize:20,
+        color:'white',
+        margin:8
+      },
+      button:{
+        // backgroundColor:'#FF7F50',
+        backgroundColor:'#FF7F50',
+        height: 40,
+        width:220,
+        padding: 10,
+        margin: 25,
+        borderRadius:15,
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      errorTxt:{
+        fontSize:10,
+        color:'#FF0D10'
+    
+      },
+      v1:{
+        flexDirection:'row',
+        marginTop:50,
+        marginBottom:0
+    
+      },
+      v2:{
+        flexDirection:'row',
+        marginTop:20,
+        height:70
+    
+      },
+      v3:{
+        marginTop:20,
+        height:70
+    
+      }
 });
 export default styles;
